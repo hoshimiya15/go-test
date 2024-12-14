e2f848c3b33de279e37db63134106f1f77abe910
hoshimiya15 <nakai.tomohisa@gmail.com>
2024-12-15 00:25:59 +0900

    fmtパッケージ

--- hello.go
+++ hello.go
@@ -5,5 +5,19 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("My", "Name", "is", "Taro")
+
+	fmt.Printf("数値=%d\n", 5)
+
+	fmt.Printf("10進数=%d 2進数=%b 8進数=%o 16進数=%x\n", 17, 17, 17, 17)
+	
+	fmt.Printf("%d年%d月%d日\n", 2015, 12)
+
+	fmt.Printf("%d年%d月%d日\n", 2015, 12, 25, 17)
+	
+	fmt.Printf("数値=%v 文字列=%v 配列=%v\n", 5, "Golang", [...]int{1, 2, 3})
+
+	fmt.Printf("数値=%#v 文字列=%#v 配列=%#v\n", 5, "Golang", [...]int{1, 2, 3})
+	
+	fmt.Printf("数値=%T 文字列=%T 配列=%T\n", 5, "Golang", [...]int{1, 2, 3})
 }
