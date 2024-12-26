a2d66c95702063ec97498a347a50e2ff87c33105
hoshimiya15 <nakai.tomohisa@gmail.com>
2024-12-26 23:25:36 +0900

    マップ

--- hello.go
+++ hello.go
@@ -2,59 +2,53 @@ package main
 
 import "fmt"
 
-func div(a, b int) (int, int) {
-	q := a / b
-	r := a % b
-	return q, r
-}
-
-func later() func(string) string {
-	var store string
-	return func(next string) string {
-		s := store
-		store = next
-		return s
-	}
-}
-
-func intergers() func() int {
-	i := 0
-	return func() int {
-		i += 1
-		return i
-	}
-}
-
 func main() {
-	// // 戻り値の破棄
-	// q, _ := div(19, 7)
+	// // マップ
+	// m := make(map[int]string)
+	// m[1] = "US"
+	// m[81] = "Japan"
+	// m[86] = "China"
+	// m[86] = "Korea" // キーの値が重複する代入を行うと要素の値は上書きされる
+	// fmt.Println(m)
+	
+	// // マップのリテラル
+	// m := map[int]string{1: "Taro", 2: "Hanako", 3: "Jiro"}
+	// fmt.Println(m)
 
-	// // 関数とエラー処理
-	// result, err := doSomething()
-	// if (err != nil) {
-	// 	// エラー処理
+	// m := map[int]string{
+	// 	1: "Taro", 
+	// 	2: "Hanako", 
+	// 	3: "Jiro",
 	// }
+	// fmt.Println(m)
 
-	// // クロージャとしての無名関数
-	// f := later()
+	// m := map[int][]int{
+	// 	1: []int{1},
+	// 	2: []int{1, 2}, 
+	// 	3: []int{1, 2, 3},
+	// }
+	// fmt.Println(m)
 
-	// fmt.Println(f("Golang"))
-	// fmt.Println(f("is"))
-	// fmt.Println(f("awesome!"))
+	// m := map[int][]int{
+	// 	1: {1},
+	// 	2: {1, 2}, 
+	// 	3: {1, 2, 3},
+	// }
+	// fmt.Println(m)
 
-	// クロージャによるジェネレータの実装
-	ints := intergers()
+	// 要素の参照
+	m := map[int]string{
+		1: "A",
+		2: "B", 
+		3: "C",
+	}
 
-	fmt.Println(ints())
-	fmt.Println(ints())
-	fmt.Println(ints())
+	// s := m[1]
+	// t := m[9]
+	// fmt.Println(s, t)
 
-	otherInts := intergers()
-	fmt.Println(otherInts())
+	s, ok := m[1]
+	t, ok := m[9]
+	_, ok := m[3]
+	fmt.Println(s, t)
 }
-
-// // 戻り値を表す変数
-// func doSomething() (x, y int) {
-// 	y = 5
-// 	return // x == 0, y == 5
-// }
