8ae99c8966b59109f46f323d901bda22e700096f
nakai.tomohisa <nakai.tomohisa@fujitsu.com>
2025-01-08 18:11:33 +0900

    チャネルとselect

--- hello.go
+++ hello.go
@@ -2,33 +2,43 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func receive(name string, ch <-chan int) {
-	for {
-		i, ok := <-ch
-		if ok == false {
-			break
-		}
-		fmt.Println(name, i)
-	}
-	fmt.Println(name + " is done.")
-}
-
 func main() {
-	ch := make(chan int, 20)
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	ch3 := make(chan int)
 
-	go receive("1st goroutine", ch)
-	go receive("2nd goroutine", ch)
-	go receive("3rd goroutine", ch)
+	// ch1から受信した整数を2倍してch2に送信
+	go func() {
+		for {
+			i := <-ch1
+			ch2 <- (i * 2)
+		}
+	}()
+
+	// ch2から受信した整数を1減算してch3に送信
+	go func() {
+		for {
+			i := <-ch2
+			ch2 <- (i - 1)
+		}
+	}()
 	
-	i := 0
-	for i < 100 {
-		ch <- i
-		i++
+	n := 1
+	LOOP:
+	for{
+		select {
+		// 整数を増分させつつch1へ送信
+		case ch1 <- n:
+			n++
+		// ch3から受信した整数を出力
+		case i := <-ch3:
+			fmt.Println("received", i)
+		default:
+			if n > 100 {
+				break LOOP
+			}
+		}
 	}
-	close(ch)
-
-	time.Sleep(3 * time.Second)
 }
