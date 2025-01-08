4f573a3436fdfe577ef313175901d2d4c460fe8a
nakai.tomohisa <nakai.tomohisa@fujitsu.com>
2025-01-08 18:03:18 +0900

    ゴルーチンとclose

--- hello.go
+++ hello.go
@@ -1,39 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func receiver(ch <-chan int) {
+func receive(name string, ch <-chan int) {
 	for {
-		i := <-ch
-		fmt.Println(i)
+		i, ok := <-ch
+		if ok == false {
+			break
+		}
+		fmt.Println(name, i)
 	}
+	fmt.Println(name + " is done.")
 }
 
 func main() {
-	// // # チャネルの型
-	// // 変数chはint型のチャネル
-	// var ch chan int
-	// // 変数ch1はint型の受信専用チャネル
-	// var ch1 <-chan int
-	// // 変数chはint型の送信専用チャネル
-	// var ch2 chan<- int
+	ch := make(chan int, 20)
 
-	// // # チャネルの生成
-	// // 変数chはバッファサイズ0のチャネル
-	// ch := make(chan int)
-	// // 変数chはバッファサイズ8のチャネル
-	// ch := make(chan int, 8)
-	// // チャネルに整数5を送信
-	// ch <- 5
-	// // チャネルから整数値を受信
-	// i := <-ch
+	go receive("1st goroutine", ch)
+	go receive("2nd goroutine", ch)
+	go receive("3rd goroutine", ch)
 	
-	// # チャネルとゴルーチン
-	ch := make(chan int)
-	go receiver(ch)
 	i := 0
-	for i < 1000 {
+	for i < 100 {
 		ch <- i
 		i++
 	}
+	close(ch)
+
+	time.Sleep(3 * time.Second)
 }
