7f6cbca1522792c4f548a1015eeda2b0fc73fbf5
nakai.tomohisa <nakai.tomohisa@fujitsu.com>
2025-03-19 17:08:17 +0900

    パッケージ

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-test
 
 go 1.23.4
+
+require github.com/tenntenn/greeting v1.0.0 // indirect
--- hello.go
+++ hello.go
@@ -2,43 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/tenntenn/greeting"
 )
 
 func main() {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
-	ch3 := make(chan int)
-
-	// ch1から受信した整数を2倍してch2に送信
-	go func() {
-		for {
-			i := <-ch1
-			ch2 <- (i * 2)
-		}
-	}()
-
-	// ch2から受信した整数を1減算してch3に送信
-	go func() {
-		for {
-			i := <-ch2
-			ch2 <- (i - 1)
-		}
-	}()
-	
-	n := 1
-	LOOP:
-	for{
-		select {
-		// 整数を増分させつつch1へ送信
-		case ch1 <- n:
-			n++
-		// ch3から受信した整数を出力
-		case i := <-ch3:
-			fmt.Println("received", i)
-		default:
-			if n > 100 {
-				break LOOP
-			}
-		}
-	}
+	greet := greeting.Do()
+	fmt.Println(greet)
 }
